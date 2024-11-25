110fe296ae70a9ee785edd5ac9f09686395e7e0c
Simran-Kaur1996 <simran31kaur@gmail.com>
2024-11-25 14:22:38 -0500

    created new files

---
+++ main.tf
@@ -0,0 +1,18 @@
+# configure aws provider
+provider "aws" {
+  region = "us-east-1"
+  profile = "terraform-user"
+}
+
+# stores the terraform state file in S3
+terraform {
+  backend "s3" {
+    bucket = "simran-terraform-remote-state"
+    key    = "terraform.tfstate.dev"
+    region = "us-east-1"
+     profile = "terraform-user"
+  }
+}
+
+
+
